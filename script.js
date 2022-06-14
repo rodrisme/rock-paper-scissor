a433bd17470c490509786bf312557bca41a6a809
Rodrigo <rodrisme@gmail.com>
2022-06-14 16:52:39 -0300

    Incomplete version of the game

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Global variables
 let computerScore = 0;
 let playerScore = 0;
+let draws = 0;
 let rps = ["rock", "paper", "scissors"];
 
 //Computer random choice
@@ -8,49 +9,50 @@ function computerPlay() {
   return rps[Math.floor(Math.random() * rps.length)];
 }
 
-// One round of RPS
-function playRound(playerSelection, computerSelection) {
+const rock = document.querySelector("#rock");
+rock.addEventListener("click", () => {
+  const playerSelection = "rock";
+  const computerSelection = computerPlay();
   if (playerSelection === computerSelection) {
-    console.log("It's a tie!");
+    let tie = document.querySelector("#draws");
+    tie.innerHTML = draws++;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    computerScore++;
-    console.log("You Lose! Paper beats Rocks");
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    playerScore++;
-    console.log("You Win! Rock beats Scissors");
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    playerScore++;
-    console.log("You Win! Paper beats Rock");
+    let scoreComputer = document.querySelector("#computer-score");
+    scoreComputer.innerHTML = computerScore++;
+  } else {
+    let scorePlayer = document.querySelector("#player-score");
+    scorePlayer.innerHTML = playerScore++;
+  }
+});
+
+const paper = document.querySelector("#paper");
+paper.addEventListener("click", () => {
+  const playerSelection = "paper";
+  const computerSelection = computerPlay();
+  if (playerSelection === computerSelection) {
+    let tie = document.querySelector("#draws");
+    tie.innerHTML = draws++;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    computerScore++;
-    console.log("You Lose! Scissors beats Paper");
+    let scoreComputer = document.querySelector("#computer-score");
+    scoreComputer.innerHTML = computerScore++;
+  } else {
+    let scorePlayer = document.querySelector("#player-score");
+    scorePlayer.innerHTML = playerScore++;
+  }
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener("click", () => {
+  const playerSelection = "scissors";
+  const computerSelection = computerPlay();
+  if (playerSelection === computerSelection) {
+    let tie = document.querySelector("#draws");
+    tie.innerHTML = draws++;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    computerScore++;
-    console.log("You Lose! Rock beats Scissors");
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerScore++;
-    console.log("You Win! Scissors beats Paper");
+    let scoreComputer = document.querySelector("#computer-score");
+    scoreComputer.innerHTML = computerScore++;
+  } else {
+    let scorePlayer = document.querySelector("#player-score");
+    scorePlayer.innerHTML = playerScore++;
   }
-}
-
-// Checks who is the winner
-function checkWinner() {
-  if (playerScore === computerScore) {
-    return "Final Result: It's a tie!";
-  } else if (playerScore > computerScore) {
-    return "Final Result: Player win!";
-  } else if (playerScore < computerScore) {
-    return "Final Result: Computer win!";
-  }
-}
-
-// Play 5 rounds of RPS
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
-    let computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-  }
-}
-game();
-console.log(checkWinner());
+});
